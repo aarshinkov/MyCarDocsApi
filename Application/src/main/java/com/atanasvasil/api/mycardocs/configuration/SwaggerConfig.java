3e79d401d64ad81f5aba9f16bbdecb51b92b7fe0
Atanas Arshinkov <a.arshinkov97@gmail.com>
2021-02-23 03:55:37 +0200

    Updated version

--- Application/src/main/java/com/atanasvasil/api/mycardocs/configuration/SwaggerConfig.java
+++ Application/src/main/java/com/atanasvasil/api/mycardocs/configuration/SwaggerConfig.java
@@ -38,7 +38,7 @@ public class SwaggerConfig {
                 .contact(new Contact("Atanas Yordanov Arshinkov", "http://github.com/aarshinkov", "a.arshinkov97@gmail.com"))
                 .license("Apache 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-                .version("1.1.1")
+                .version("1.2.1")
                 .build();
     }
 }
